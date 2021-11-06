e7cc9297c491b0f010700d59cdeb5f2c40336986
thallard <thallard@student.42lyon.fr>
2021-11-06 10:33:25 +0100

    cadeau

---
+++ srcs/Game.py
@@ -0,0 +1,18 @@
+import sys
+
+
+class Game(object):
+
+    # TODO : game params
+    def __init__(self, size):
+        self.size = size
+
+
+    def basic_func(self, val):
+        print("je suis une function " + str(val))
+        return 1
+
+
+
+game = Game(size=1000)
+game.basic_func(42)
