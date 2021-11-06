f44adc09dff43a7e54fff052cb28fbc91faa4b52
thallard <thallard@student.42lyon.fr>
2021-11-06 11:04:56 +0100

    Added printer function and gitlab submodule

--- srcs/main.py
+++ srcs/main.py
@@ -1,21 +1,32 @@
-import sys
 from Game import Game
 
-width = input()
-height = input()
-win_length = input()
-play_order = input()
-game_status = 0;
-t_time = 10000
-g_time = 100
 
-# TODO : 
-#   . Creation of tab
+# Basic printer, used to say which column we played
+def printer(value):
+    print(value)
 
-tab = 0;
 
-game = Game(tab, height, width, win_length, t_time, g_time, game_status)
+# Main function
+def main():
+    width = input()
+    height = input()
+    win_length = input()
+    play_order = input()
+    game_status = 0
+    t_time = 10000
+    g_time = 100
 
-while (game.game_status):
-	test = input
-	print(test)
+    # TODO :
+    #   . Creation of tab
+
+    tab = 0
+
+    game = Game(tab, height, width, win_length, t_time, g_time, game_status)
+
+    while game.game_status:
+        test = input
+        print(test)
+
+
+if __name__ == "__main__":
+    main()
